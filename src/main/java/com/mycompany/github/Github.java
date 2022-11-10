6071f4d7238f67d54fb9c19c3b004cb11178e48d
Stanislav <konoku@rambler.ru>
2022-11-10 19:36:36 +0700

    Грибы начало

--- src/main/java/com/mycompany/github/Github.java
+++ src/main/java/com/mycompany/github/Github.java
@@ -11,7 +11,16 @@ package com.mycompany.github;
  */
 public class Github {
 
+    
+    
+    
+    public static int Summ(int a, int b){
+        return a+b;
+    }
+    
+    
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Mushroom World!");
     }
 }
